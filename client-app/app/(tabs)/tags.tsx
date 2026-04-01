dd266fbc551536ca7611d3d03c09ad7c2bc6275b
Troy Lu <u1509365@utah.edu>
2026-04-01 14:06:00 -0600

    add sqlite local db demo

--- client-app/app/(tabs)/tags.tsx
+++ client-app/app/(tabs)/tags.tsx
@@ -1,21 +1,71 @@
-import { Text, View, StyleSheet } from "react-native";
+import { addTag, deleteTag, getAllTags, Tag } from "@/lib/localdb";
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { Text, View, StyleSheet, TextInput, Button } from "react-native";
 
 export default function TagsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Tags Screen</Text>
-    </View>
-  );
+    const db = useSQLiteContext();
+
+    const [newTagName, setNewTagName] = useState("");
+    const [tags, setTags] = useState<Tag[]>([]);
+
+    // initialize list of tags when first rendering this component
+    useEffect(() => {
+        getAllTags(db).then(setTags);
+    }, []);
+
+    async function onAddTag() {
+        await addTag(db, newTagName);
+        const newTags = await getAllTags(db);
+        setTags(newTags);
+    }
+
+    async function onDeleteTag(id: number) {
+        await deleteTag(db, id);
+        const newTags = await getAllTags(db);
+        setTags(newTags);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Tags Screen</Text>
+
+            <View style={styles.rowContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="new tag name..."
+                    onChangeText={setNewTagName}
+                />
+                <Button title="add" onPress={onAddTag} />
+            </View>
+
+            <Text style={styles.text}>Your tags:</Text>
+            <View>
+                {tags.map((tag) => (
+                    <View key={tag.tid} style={styles.rowContainer}>
+                        <Text style={styles.text}>{tag.name}</Text>
+                        <Button title="x" onPress={() => onDeleteTag(tag.tid)} />
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    color: "#fff",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#25292e",
+    },
+    text: {
+        color: "#fff",
+    },
+    rowContainer: {
+        flexDirection: "row",
+    },
+    textInput: {
+        outlineColor: "white",
+        outlineWidth: 2,
+        padding: 4
+    }
 });
--- client-app/app/_layout.tsx
+++ client-app/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { initDb } from "@/lib/localdb";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <SQLiteProvider databaseName="cadenzaLocalDb" onInit={initDb}>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </SQLiteProvider>
+    );
 }
