027f9af903bcdb1d5a42dab39ff5a643da47fee5
Kevin Cuellar <kevcuellar2003@gmail.com>
2026-04-17 17:08:48 -0600

    fix SafeAreaView import

--- client-app/src/app/(tabs)/query.tsx
+++ client-app/src/app/(tabs)/query.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { SafeAreaView, ActivityIndicator, StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { QueryBuilder } from "../../features/query-builder/QueryBuilder";
 import { useAccount } from "@/lib/account";
 import { useTags } from "@/lib/tags";
