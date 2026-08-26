8cb7434c4652fc1e877292cd164528832c3bc5c5
David Cosby <davjcosby@gmail.com>
2026-08-25 19:40:21 -0600

    prettier formatting for the react native stuff

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,10 +5,14 @@
 #
 
 RUST_DIR="./backend-api"
+REACT_NATIVE_DIR="./client-app"
+
 RUST_FMT_ARGS=""
+PRETTIER_ARGS="--write"
 
 if [ "$1" == "--check" ]; then
   RUST_FMT_ARGS="--check"
+  PRETTIER_ARGS="--check"
 fi
 
 echo "----- 1. Rust format pass for files in $RUST_DIR -----"
@@ -17,5 +21,15 @@ if cargo fmt --manifest-path "$RUST_DIR/Cargo.toml" $RUST_FMT_ARGS; then
   echo "✅ cargo fmt succeeded."
 else
   echo "❌ cargo fmt failed!"
+  exit 1
+fi
+
+echo ""
+echo "----- 2. Prettier format pass for files in $REACT_NATIVE_DIR -----"
+
+if npx prettier $REACT_NATIVE_DIR $PRETTIER_ARGS; then
+  echo "✅ Prettier succeeded."
+else
+  echo "❌ Prettier failed!"
   exit 1
 fi
